5007136d81c0fef300f6b1f74840ad9ad418ecd5
xuanvu <xuanvuaudi2002@gmail.com>
2025-10-11 23:00:25 +0700

    none 1

--- src/main/java/utils/SimpleDocumentListener.java
+++ src/main/java/utils/SimpleDocumentListener.java
@@ -3,6 +3,7 @@ package utils;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
+// helper class để lắng nghe sự thay đổi của JTextField hoặc bất kỳ Document nào trong Swing.
 public class SimpleDocumentListener implements DocumentListener {
     private final Runnable onChange;
 
